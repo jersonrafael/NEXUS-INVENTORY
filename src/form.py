ce5914b235f9dabd24b02b0622358e9a873b514a
Jerson <jrrvgamer@gmail.com>
2023-09-21 17:56:06 -0400

    Update

--- src/form.py
+++ src/form.py
@@ -1,12 +1,32 @@
 from flask_wtf import FlaskForm 
-from wtforms import StringField,SubmitField,validators,FloatField,IntegerField,SelectField
+from wtforms import StringField,SubmitField,validators,FloatField,IntegerField,SelectField,FileField
 from wtforms.validators import DataRequired
+from flask_wtf.file import FileField, FileRequired
+from wtforms import ValidationError
 
+from conn import categorys
 
-class agregar(FlaskForm):
-    p_name = StringField(label=('Nombre de producto:'), validators=[DataRequired()])
-    p_price = FloatField(label=('Precio de producto:'), validators=[DataRequired()])
-    p_quantity = IntegerField(label=('Cantidad del producto:'), validators=[DataRequired()])
-    p_description = StringField(label=('Descripcion del producto'), validators=[DataRequired()])
-    category = SelectField(u'Programming Language', choices=[('cpp', 'C++'), ('py', 'Python'), ('text', 'Plain Text')])
+find_cat = categorys.find()
+
+def validate_no_numbers(form, field):
+    # Esta función verificará si el campo contiene números
+    if any(char.isdigit() for char in field.data):
+        raise ValidationError('Este campo no debe contener números')
+
+class Producto(FlaskForm):
+    p_name = StringField(label=('Nombre de producto:'), validators=[DataRequired(message='Campo obligatorio'), validate_no_numbers])
+    p_price = FloatField(label=('Precio de producto en USD:'), validators=[DataRequired(message='Campo obligatorio')])
+    p_quantity = IntegerField(label=('Cantidad del producto:'), validators=[DataRequired(message='Campo obligatorio')])
+    p_description = StringField(label=('Descripcion del producto'), validators=[DataRequired(message='Campo obligatorio'), validate_no_numbers])
+    category = SelectField(u'Categoria del producto', choices=[(x['_id'], x['cat_name']) for x in find_cat])
+    ImagenProducto = FileField(validators=[FileRequired()])
     submit = SubmitField(label=('Submit'))
+
+
+class EditProductForm(FlaskForm):
+    p_name = StringField('Nombre de producto:', validators=[DataRequired(), validate_no_numbers])
+    p_price = FloatField('Precio de producto:', validators=[DataRequired()])
+    p_quantity = IntegerField('Cantidad del producto:', validators=[DataRequired()])
+    p_description = StringField('Descripción del producto:', validators=[DataRequired()])
+    category = SelectField('Categoría del producto', choices=[], validators=[DataRequired()])
+    submit = SubmitField('Guardar Cambios')
--- src/routes/product.py
+++ src/routes/product.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for,request
+from flask import flash, render_template, redirect, url_for,request
 # from werkzeug import secure_filename
 from app import app
 
@@ -7,14 +7,21 @@ from conn import products,categorys
 
 from cloud import cloudinary
 
+from form import Producto,EditProductForm
+
 import os
+
+from priceBs import priceVes
+
+priceVes = priceVes.replace(',', '.')
 
 #SEE ALL INFO OF THE PRODUCT
 @app.route('/product/<p_id>')
 def moreInfo(p_id):
     p_fund = products.find_one({'_id': ObjectId(p_id)})
-    cat_fund = categorys.find_one({'_id':ObjectId(p_id)})
-    return render_template('productInfo.html', p_fund=p_fund,cat_fund=cat_fund)
+    idCat = p_fund['cat_id']
+    cat_fund = categorys.find_one({'_id':ObjectId(idCat)})
+    return render_template('productInfo.html', p_fund=p_fund,cat_fund=cat_fund,bcv=priceVes)
 
 # DELETE A PRODUCT
 @app.route('/del/<_id>', methods=['GET'])
@@ -27,13 +34,13 @@ def delete_product(_id):
         return redirect(url_for('inventario'))
     
 # EDIT A PRODUCT
-@app.route('/edit/<_id>', methods=['GET','POST'])
+@app.route('/edit/<_id>', methods=['GET', 'POST'])
 def edit_product(_id):
-
     if request.method == 'GET':
         p = products.find_one({"_id": ObjectId(_id)})
         p_info = p['cat_id']
-        p_cat = categorys.find_one({"_id": ObjectId(_id)})
+        p_cat = categorys.find_one({"_id": ObjectId(p_info)})
+        print(p_cat)
         p_cat_g = categorys.find()
         return render_template('modificar.html', p=p,p_cat=p_cat,p_cat_g=p_cat_g)
     else:
@@ -56,23 +63,24 @@ def edit_product(_id):
             p = products.find_one({"_id": ObjectId(_id)})
             return render_template('modificar.html', p=p, message=message)
         else:
-            products.update_one({"_id": ObjectId(_id)}, {"$set": {"p_name": p_name, "p_price":p_price, "p_quantity":p_quantity, "p_description":p_description, 'cat_id':cat_id}})
+            products.update_one({"_id": ObjectId(_id)}, {"$set": {"p_name": p_name, "p_price":p_price, "priceVes": int(p_price) * float(priceVes),"p_quantity":p_quantity, "p_description":p_description, 'cat_id':cat_id}})
             message = "Se han realizado los cambios con exito"
             p = products.find_one({"_id": ObjectId(_id)})
             p_info = p['cat_id']
             p_cat = categorys.find_one({"_id": ObjectId(p_info)})
             p_cat_g = categorys.find()
-            return render_template('modificar.html', p=p, message=message,p_cat=p_cat,p_cat_g=p_cat_g)
-        
+            return render_template('modificar.html', p=p, message=message,p_cat=p_cat,p_cat_g=p_cat_g,bcv=priceVes)
+
             
+
 #ADD PRODUCT
 @app.route('/agregar_producto', methods=['GET','POST'])
 def add_product():
-
+    form = Producto()
     if request.method == 'GET':
         message = ''
         find_cat = categorys.find()
-        return render_template('agg_productos.html',message=message,find_cat=find_cat)
+        return render_template('agg_productos.html',message=message,find_cat=find_cat, form=form, bcv=priceVes)
     else:
         pass 
     
@@ -82,37 +90,24 @@ def add_product():
         p_price = req_data['p_price'].capitalize()
         p_quantity = req_data['p_quantity'].capitalize()
         p_description = req_data['p_description'].capitalize()
-        cat_id = req_data['_id']
+        cat_id = req_data['category']
 
-        file = request.files['file']
+        file = request.files['ImagenProducto']
         filen = file.filename
-        print(filen)
-
-        if filen == '':
-            message = 'Selecciona una imagen para el producto'
-            find_cat = categorys.find()
-            return render_template('agg_productos.html',message=message, find_cat=find_cat)
-        else:
-            pass
-        product_data = {"p_name": p_name, "p_price": p_price, "p_quantity": p_quantity,
-                        "p_description": p_description, "cat_id":cat_id, 'file_name':filen}
-
-        # Check if exist a document with the same name
-        result = products.find_one({"p_name": p_name})
-        if result is not None:
-            message = 'El producto ya existe'
-            find_cat = categorys.find()
-            return render_template('agg_productos.html',message=message, find_cat=find_cat)
-        else:
-            if p_name and p_description and p_price and p_quantity and filen != '':
+        if form.validate_on_submit(): 
+            product_data = {"p_name": p_name, "p_price": p_price,"priceVes": int(p_price) * float(priceVes), "p_quantity": p_quantity,"p_description": p_description, "cat_id":cat_id, 'file_name':filen}
+            result = products.find_one({"p_name": p_name})
+            if result is not None:
+                message = 'El producto ya existe'
                 find_cat = categorys.find()
-                products.insert_one(product_data)
-                cloudinary.uploader.upload(file, public_id=filen)
-                message = 'Producto agregado'
-                return render_template('agg_productos.html',message=message,find_cat=find_cat)
+                return render_template('agg_productos.html',message=message, find_cat=find_cat,form=form,bcv=priceVes)
             else:
                 find_cat = categorys.find()
-                message = 'Producto no agregado revisa que todos los campos esten correctos'
-                product_data = {"p_name": p_name, "p_price": p_price,"p_quantity": p_quantity,
-                        "p_description": p_description, "cat_id":cat_id, 'file_name':filen}
-                return render_template('agg_productos.html',message=message, product_data=product_data, find_cat=find_cat)
+                products.insert_one(product_data)
+                # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filen))
+                cloudinary.uploader.upload(file, public_id=filen)
+                message = 'Producto agregado'
+                return render_template('agg_productos.html',message=message,find_cat=find_cat,form=form,bcv=priceVes)
+        message = 'Error al agregar el producto revisa que los campos este correctos'
+        return render_template('agg_productos.html',message=message,form=form)
+
